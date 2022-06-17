e7f4cf186a91c8f8eca1bcb4aa9115ed55b07249
binamralamsal <binamralamsal@gmail.com>
2022-06-17 14:41:00 +0545

    Added mongodb

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express, { Application } from "express";
+import { connect, set } from "mongoose";
 
 import Routes from "./interfaces/routes.interface";
 import errorMiddleware from "./middlewares/error.middleware";
@@ -7,11 +8,14 @@ class App {
   public app: Application;
   public env: string;
   public port: string | number;
+  private readonly mongodbUri: string;
 
   constructor(routes: Routes[]) {
     this.app = express();
     this.env = process.env.NODE_ENV || "development";
     this.port = process.env.PORT || 4000;
+    this.mongodbUri =
+      process.env.MONGODB_URI || "mongodb://127.0.0.1/learn-node";
 
     this.initializeMiddlewares();
     this.initializeRoutes(routes);
@@ -19,12 +23,26 @@ class App {
   }
 
   public async listen() {
+    await this.connectToMongodb();
+
     this.app.listen(this.port, () => {
       console.log(`=================================`);
       console.log(`======= ENV: ${this.env} =======`);
       console.log(`🚀 App listening on the port ${this.port}`);
       console.log(`=================================`);
     });
+  }
+
+  private async connectToMongodb() {
+    try {
+      if (this.env !== "production") {
+        set("debug", true);
+      }
+
+      await connect(this.mongodbUri);
+    } catch (error) {
+      process.exit(0);
+    }
   }
 
   private initializeMiddlewares() {
